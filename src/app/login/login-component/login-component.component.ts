1ddd8c35c6dacfa1b8632bc9f7aece612ee65d50
SugataMaji <sugatamaji94@gmail.com>
2017-08-06 01:22:34 +0530

    Component - Assignment 5.4

---
+++ src/app/login/login-component/login-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
